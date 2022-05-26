fdf8c14169bd7e98052837f3c43d1a4ee371fded
Sanskar Saraf <sarafsanskar468@gmail.com>
2022-05-26 14:20:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function display()
+  {
+    var input = document.getElementById('input')
+    if(input.value == "")
+    {  
+      document.getElementById('error').innerHTML = "Please Enter your task"
+      document.getElementById('input').style.borderColor = "red"
+      document.getElementById('error').style.color = "red"
+    }else{
+      var li = document.createElement('li')
+      li.textContent = input.value
+      list.appendChild(li,list)
+      input.innerHTML = "";
+    }
+  }
